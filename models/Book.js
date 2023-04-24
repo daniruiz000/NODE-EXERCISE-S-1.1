0f431bfda7019e05e2819e439956468eb43650ae
Daniel Ruiz <danielruimar@gmail.com>
2023-04-24 20:38:45 +0200

    add exercise

---
+++ models/Book.js
@@ -0,0 +1,18 @@
+//  Importamos Mongoose
+const mongoose = require("mongoose");
+// Declaramos nuestro esquema que nos permite declarar nuestros objetos y crearle restricciones.
+const Schema = mongoose.Schema;
+// Creamos esquema del usuario
+const bookSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    pages: { type: Number, required: false },
+    //
+  },
+  { timestamps: true } // Cada vez que se modifique un documento refleja la hora y fecha de modificación
+);
+// Creamos un modelo parea que siempre que creamos un usuario valide contra el Schema que le hemos creado para ver si es valido.
+const Book = mongoose.model("Book", bookSchema);
+//  Exportamos para poder usarlo fuera
+module.exports = { Book };
---
+++ seeds/book.seed.js
@@ -0,0 +1,49 @@
+//  Importamos mongoose
+const mongoose = require("mongoose");
+//  Nos conectamos a la BBDD
+const { connect } = require("../db.js");
+// Importamos el modelo
+const { Book } = require("../models/Book.js");
+//
+const bookList = [
+  {
+    title: "Harry Potter",
+    author: "J.K. Rowling",
+    pages: 543,
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    pages: 328,
+  },
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    pages: 281,
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    pages: 180,
+  },
+  {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    pages: 279,
+  },
+];
+//  Cuando nos conectemos ...
+connect().then(() => {
+  console.log("Tenemos conexión");
+  // Elimina los datos previos
+  Book.collection.drop().then(() => {
+    console.log("Usuarios eliminados");
+
+    //  Añadimos los usuarios
+    const documents = bookList.map((user) => new Book(user));
+    Book.insertMany(documents) // Insertamos nuestros documentos
+      .then(() => console.log("Datos guardados correctamente!")) // Si ha ido bien imprimo por consola
+      .catch((error) => console.error(error)) // Si hay error imprimo error
+      .finally(() => mongoose.disconnect()); // Cierro la conexion
+  });
+});
